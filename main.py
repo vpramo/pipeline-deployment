90dbd3f1fe7c1179a83a0f1a6a76d1face002dd4
vpramo <pramod.venkatesh@ril.com>
2015-11-30 12:43:55 +0530

    Added More features

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import httplib
 import requests
 import time
 import yaml
+import gnupg
+import re
+from tempfile import NamedTemporaryFile
 
 authtoken=sys.argv[1]
-#Example  https://aasemble.com/
+#Example  https://aasemble.com
 pkgbuilder_url=sys.argv[2]
 search_tag=sys.argv[3]
+
+gpg=gnupg.GPG(gnupghome='/tmp/')
 
 def make_requests(api_path):
     Headers2 = {'Authorization':'Token '+ authtoken}
@@ -31,6 +36,28 @@ def get_snapshots():
     snapshots=make_requests(api_path)
     return snapshots["results"]
 
+def get_keys(key_url):
+    r = requests.get(key_url)
+    if r.status_code == requests.codes.ok:
+        f = NamedTemporaryFile()
+        f.write(r.content)
+        f.seek(0)
+        key_val=gpg.scan_keys(f.name)
+        f.close()
+        keyid=key_val[0]["keyid"]
+        return keyid
+    else:
+        return "no keys"
+
+def get_repo_properties(repo_url,series):
+    r = requests.get(repo_url +"/dists/"+series+"/Release")
+    if r.status_code == requests.codes.ok:
+        origin=re.search("Origin: (\w+)",r.content).group(1)
+        label=re.search("Label: (\w+)",r.content).group(1)
+        return [origin,label]
+    else:
+       raise "Invalid URL"
+
 def output_yaml():
     snapshot_list=get_snapshots()
     required_snapshots=[]
@@ -46,14 +73,25 @@ def output_yaml():
             m = make_requests(mirror)
             name = str(m["url"].replace("http://","").replace("/","_"))
             repo_list[name]={}
-            repo_list[name]["location"]=str(m["url"])
+            repo_list[name]["location"]=str(snapshot['self']).replace("/api/v2","")+str(m["url"]).replace("http://","").replace("https://","")
             repo_list[name]["release"]=str(" ".join(m["series"]))
             repo_list[name]["repos"]=str(" ".join(m["components"]))
-            repo_list[name]["pin"]='1002'
-            print yaml.dump(repo_list, default_flow_style=False)
+            repo_list[name]["pin"]={}
+            for series in m["series"]:
+                repo_prop=get_repo_properties(str(m["url"]),str(series))
+                repo_list[name]["pin"][str(series)+repo_prop[1]]={}
+                repo_list[name]["pin"][str(series)+repo_prop[1]]["originator"]=repo_prop[0]
+                repo_list[name]["pin"][str(series)+repo_prop[1]]["label"]=repo_prop[1]
+                repo_list[name]["pin"][str(series)+repo_prop[1]]["priority"]='1001'
+            keyid=get_keys(str(m["url"])+'/repo.key')
+            if keyid != "no keys":
+                repo_list[name]["key"]={}
+                repo_list[name]["key"]["id"]=keyid
+                repo_list[name]["key"]["source"]=str(m["url"])+'/repo.key'
+
+    print " " + str(yaml.dump(repo_list, default_flow_style=False)).rstrip("\n")
 
 output_yaml()
-
 
 
 
